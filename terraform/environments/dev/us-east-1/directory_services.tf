38e3c742b8cd29beedd157b48e2cb133a20e624c
John Mason <cojohn84@gmail.com>
2024-01-08 12:23:51 -0500

    commented out the fsx.tf and directory_services.tf files

--- terraform/environments/dev/us-east-1/directory_services.tf
+++ terraform/environments/dev/us-east-1/directory_services.tf
@@ -5,18 +5,18 @@
 ########## Managed Active Directory ##########
 ##### Uncomment if customer is using Managed Active Directory #####
 
-resource "aws_directory_service_directory" "workspaces_mad" {
-  name        = var.mad_fqdn
-  password    = "${random_password.aws_directory_services_managed_ad_password.result}"
-  edition     = var.mad_edition
-  type        = var.mad_type
-  short_name  = var.mad_netbios
+# resource "aws_directory_service_directory" "workspaces_mad" {
+#   name        = var.mad_fqdn
+#   password    = "${random_password.aws_directory_services_managed_ad_password.result}"
+#   edition     = var.mad_edition
+#   type        = var.mad_type
+#   short_name  = var.mad_netbios
 
-  vpc_settings {
-    vpc_id     = module.workspaces_vpc.id
-    subnet_ids = [module.workspaces_vpc.private_subnet_ids[(var.vpc_availability_zone_1)], module.workspaces_vpc.private_subnet_ids[(var.vpc_availability_zone_2)]]
-  }
-}
+#   vpc_settings {
+#     vpc_id     = module.workspaces_vpc.id
+#     subnet_ids = [module.workspaces_vpc.private_subnet_ids[(var.vpc_availability_zone_1)], module.workspaces_vpc.private_subnet_ids[(var.vpc_availability_zone_2)]]
+#   }
+# }
 
 ##### AD Connector ######
 
--- terraform/environments/dev/us-east-1/fsx.tf
+++ terraform/environments/dev/us-east-1/fsx.tf
@@ -4,20 +4,20 @@
 
 ########## FSx for Windows File System Managed AD ##########
 
-resource "aws_fsx_windows_file_system" "fsx_managed_ad" {
-  active_directory_id = aws_directory_service_directory.workspaces_mad.id
-  kms_key_id          = module.kms.fsx_key_arn
-  storage_capacity    = var.fsx_storage_capacity
-  subnet_ids          = [module.workspaces_vpc.private_subnet_ids[(var.vpc_availability_zone_1)], module.workspaces_vpc.private_subnet_ids[(var.vpc_availability_zone_2)]]
-  throughput_capacity = var.fsx_throughput_capacity
-  security_group_ids  = [aws_security_group.fsx_sg.id]
-  deployment_type     = var.fsx_deployment_type
-  preferred_subnet_id = module.workspaces_vpc.private_subnet_ids[(var.vpc_availability_zone_1)]
- # aliases             = var.fsx_aliases
-  tags = {
-    Name = var.fsx_name
-  }
-}
+# resource "aws_fsx_windows_file_system" "fsx_managed_ad" {
+#   active_directory_id = aws_directory_service_directory.workspaces_mad.id
+#   kms_key_id          = module.kms.fsx_key_arn
+#   storage_capacity    = var.fsx_storage_capacity
+#   subnet_ids          = [module.workspaces_vpc.private_subnet_ids[(var.vpc_availability_zone_1)], module.workspaces_vpc.private_subnet_ids[(var.vpc_availability_zone_2)]]
+#   throughput_capacity = var.fsx_throughput_capacity
+#   security_group_ids  = [aws_security_group.fsx_sg.id]
+#   deployment_type     = var.fsx_deployment_type
+#   preferred_subnet_id = module.workspaces_vpc.private_subnet_ids[(var.vpc_availability_zone_1)]
+#  # aliases             = var.fsx_aliases
+#   tags = {
+#     Name = var.fsx_name
+#   }
+# }
 
 ########## FSx for Windows File System For AD Connector ##########
 
